7b8e8c7fdca755338ef21bce3b11b41e6767f7a1
dome4 <dominic.seitz@gmx.de>
2018-05-06 12:41:58 +0200

    office ui fabric use updated

--- VersionControl2Web/src/app/app.component.ts
+++ VersionControl2Web/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { OnInit, AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+// office-ui-fabric variable
+declare let fabric: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'app';
+
+
+  ngAfterViewInit() {
+
+    var DropdownHTMLElements = document.querySelectorAll('.ms-Dropdown');
+    for (var i = 0; i < DropdownHTMLElements.length; ++i) {
+      let Dropdown = new fabric['Dropdown'](DropdownHTMLElements[i]);
+    }
+  }
+
+  
+
+
+
 }
