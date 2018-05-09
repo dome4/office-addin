a7759533ae043834b8f007ece52e392d713c4a01
dome4 <dominic.seitz@gmx.de>
2018-05-09 11:51:06 +0200

    test get request

--- VersionControl2Web/src/app/app.component.ts
+++ VersionControl2Web/src/app/app.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { RequirementService } from './services/requirement.service';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Requirement } from './models/requirement';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit{
+
+  public requirements: Requirement[] = [];
+
+
+  constructor(private reqService: RequirementService) { }
+
+
+  ngOnInit() {
+    this.reqService.getRequirements()
+      .subscribe(requirements => {
+
+        console.log('Request');
+        console.log(requirements);
+
+        this.requirements = requirements;
+    });
+  }
+}
--- VersionControl2Web/src/app/services/requirement.service.ts
+++ VersionControl2Web/src/app/services/requirement.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Requirement } from '../models/requirement';
 
-const api = '/api';
+const api = 'http://localhost:3000/api';
 
 @Injectable()
 export class RequirementService {
