055f30efc2721fcc2c2851d64809a97d7a9d5b82
dome4 <dominic.seitz@gmx.de>
2018-05-28 19:01:55 +0200

    main.ts updated

--- VersionControl2Web/src/main.ts
+++ VersionControl2Web/src/main.ts
@@ -12,12 +12,12 @@ declare const Office: any;
 
 Office.initialize = (reason: any) => {
 
-  console.log('Version Control: initializing office.js...');
+  // console.log('Version Control: initializing office.js...');
 
   // bootstrap
   platformBrowserDynamic().bootstrapModule(AppModule)
     .then((success: any) => {
-      console.log('Version Control: bootstrap success', success);
+      // console.log('Version Control: bootstrap success', success);
     })
     .catch((error: any) => {
       console.log('Version Control: bootstrap error', error);
