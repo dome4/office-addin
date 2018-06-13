91aeaae59aa8c10b33f5292d4d11cf4cf48ad226
dome4 <dominic.seitz@gmx.de>
2018-06-13 12:03:36 +0200

    useHash to work with office.js script

--- VersionControl2Web/src/app/app-routing.module.ts
+++ VersionControl2Web/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RequirementComponent } from './requirement/requirement.component';
+import { RequirementListComponent } from './requirement-list/requirement-list.component';
 
 const appRoutes: Routes = [
-  { path: '', component: RequirementComponent }
+  { path: '', component: RequirementComponent },
+  { path: 'list', component: RequirementListComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
