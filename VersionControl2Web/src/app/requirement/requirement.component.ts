42969cc4e87b32ae5b979ea9d5d7e53a80039b26
dome4 <dominic.seitz@gmx.de>
2018-06-25 15:06:22 +0200

    animations 2.0

--- VersionControl2Web/src/app/requirement/requirement.component.ts
+++ VersionControl2Web/src/app/requirement/requirement.component.ts
@@ -3,37 +3,30 @@ import { RequirementService } from '../services/requirement.service';
 import { Requirement } from '../models/requirement';
 import { Observable } from 'rxjs';
 import 'rxjs';
-import { trigger, transition, animate, state, style } from '@angular/animations';
+import { trigger, transition, animate, style, animateChild } from '@angular/animations';
 
 @Component({
   animations: [
-    trigger('nextRow', [
-      transition('void => *', [
-        style({
-          transform: 'translateY(-100px)',
-          opacity: 0
-        }),
-        animate(800, style({
-          transform: 'translateY(0)',
-          opacity: 1
-        }))
-      ])
-    ]),
     trigger('expandNodeLeft', [
       transition('void => *', [
         style({
-          transform: 'translate(100px, -50px)',
+          transform: 'translateX(100px)',
           opacity: 0
         }),
-        animate(500, style({
-          transform: 'translateY(0)',
-          opacity: 0.5
-        })),
         animate(1000, style({
           transform: 'translateX(0)',
           opacity: 1
         }))
-
+      ]),
+      transition('* => void', [
+        style({
+          transform: 'translateX(0)',
+          opacity: 1
+        }),
+        animate(1000, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
       ])
     ]),
     trigger('expandNodeRight', [
@@ -46,10 +39,28 @@ import { trigger, transition, animate, state, style } from '@angular/animations'
           transform: 'translateX(0)',
           opacity: 1
         }))
-
+      ]),
+      transition('* => void', [
+        style({
+          transform: 'translateX(0)',
+          opacity: 1
+        }),
+        animate(1000, style({
+          transform: 'translateX(-100px)',
+          opacity: 0
+        }))
       ])
     ]),
-
+    trigger('expandNodeMiddle', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 }))
+      ]),
+      transition('* => void', [
+        style({ opacity: 1 }),
+        animate(1000, style({ opacity: 0 }))
+      ])
+    ])
   ],
   selector: 'app-requirement',
   templateUrl: './requirement.component.html',
@@ -112,15 +123,3 @@ export class RequirementComponent implements OnInit {
     this.state == 'active' ? this.state = 'inactive' : this.state = 'active';
   }
 }
-
-//animations: [
-//  trigger('nextNode', [
-//    state('inactive', style({
-//      backgroundColor: 'blue'
-//    })),
-//    state('active', style({
-//      backgroundColor: 'red'
-//    })),
-//    transition('active <=> inactive', animate(1000))
-//  ])
-//],
