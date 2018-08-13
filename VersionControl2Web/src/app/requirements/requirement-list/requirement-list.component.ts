e14ddc006b8f2c09eddc744c63d12ad1b2307780
dome4 <dominic.seitz@gmx.de>
2018-08-13 18:11:23 +0200

    requirement-list-component updated

--- VersionControl2Web/src/app/requirements/requirement-list/requirement-list.component.ts
+++ VersionControl2Web/src/app/requirements/requirement-list/requirement-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreService } from '../../services/store.service';
 
 @Component({
   selector: 'app-requirement-list',
@@ -7,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequirementListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private storeService: StoreService) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 }
