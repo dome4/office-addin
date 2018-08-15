ac705e0e7dae1dff3d35b6203d97492d39de7c5f
dome4 <dominic.seitz@gmx.de>
2018-08-15 11:57:02 +0200

    functions removed from manifest and provider updated

--- VersionControl2Web/Functions/FunctionFile.js
+++
@@ -1,6 +0,0 @@
-﻿// Die Initialisierungsfunktion muss bei jedem Laden einer neuen Seite ausgeführt werden.
-(function () {
-    Office.initialize = function (reason) {
-        // Wenn eine Initialisierung erfolgen muss, kann dies hier geschehen.
-    };
-})();
