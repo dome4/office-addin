b8721ef72992d7ff4b25b4a4660811bcd58ac5c8
dome4 <dominic.seitz@gmx.de>
2018-08-16 10:30:12 +0200

    updated sign in / out process

--- VersionControl2Web/src/app/header/header.component.ts
+++ VersionControl2Web/src/app/header/header.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
+  /**
+   * sign out the current user
+   *
+   */
+  onSignOut() {
+
+    // logout
+    this.authService.logout();
+
+    // redirect to signin page
+    this.router.navigate(['signin']);
+
+    
   }
-
 }
